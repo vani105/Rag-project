5661602253f4a5039affd79b54947fd26a6d5b97
vani105 <srivaniathota@gmail.com>
2026-09-25 16:28:27 +0530

    Fix LangChain text splitter import

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 import os
 
 # Update imports for LangChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 # from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 # updated
